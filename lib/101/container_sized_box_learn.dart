1c1ae7cf5839a8b099771d94a73aa0d7f6980bcf
devop <devops.engineering06@gmail.com>
2023-01-05 08:25:30 +0300

    Container

--- lib/101/container_sized_box_learn.dart
+++ lib/101/container_sized_box_learn.dart
@@ -19,8 +19,70 @@ class ContainerSizedBoxLearn extends StatelessWidget {
             dimension: 50,
             child: Text('b' * 50),
           ),
+          Container(
+            //  color: Colors.red,
+            height: 50,
+            constraints: const BoxConstraints(
+              minWidth: 100,
+              maxWidth: 150,
+              maxHeight: 100,
+            ),
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            // decoration: BoxDecoration(
+            //   borderRadius: BorderRadius.circular(10),
+            //   color: Colors.red,
+            //   gradient:
+            //       const LinearGradient(colors: [Colors.red, Colors.black]),
+            //   boxShadow: const [
+            //     BoxShadow(
+            //         color: Colors.green, offset: Offset(0.1, 1), blurRadius: 2)
+            //   ],
+            //   // shape: BoxShape.circle,
+            //   border: Border.all(
+            //     width: 10,
+            //     color: Colors.white12,
+            //   ),
+            // ),
+            decoration: ProjectUtility.boxDecoration,
+            child: Text('aa' * 1),
+          ),
         ],
       ),
     );
   }
+}
+
+class ProjectUtility {
+  static BoxDecoration boxDecoration = BoxDecoration(
+    borderRadius: BorderRadius.circular(10),
+    color: Colors.red,
+    gradient: const LinearGradient(colors: [Colors.red, Colors.black]),
+    boxShadow: const [
+      BoxShadow(color: Colors.green, offset: Offset(0.1, 1), blurRadius: 2)
+    ],
+    // shape: BoxShape.circle,
+    border: Border.all(
+      width: 10,
+      color: Colors.white12,
+    ),
+  );
+}
+
+class ProjectContainerDecoration extends BoxDecoration {
+  ProjectContainerDecoration()
+      : super(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.red,
+          gradient: const LinearGradient(colors: [Colors.red, Colors.black]),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.green, offset: Offset(0.1, 1), blurRadius: 2)
+          ],
+          // shape: BoxShape.circle,
+          border: Border.all(
+            width: 10,
+            color: Colors.white12,
+          ),
+        );
 }
